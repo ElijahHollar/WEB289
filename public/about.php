d907e752235d2ce11d45b32c4a354bc13fb7f21d
Elijah Hollar <elijahshollar@students.abtech.edu>
2021-03-05 15:05:23 -0500

    Minor css work/code cleanup

--- public/about.php
+++ public/about.php
@@ -1,6 +1,6 @@
 <?php 
-  require_once('../private\initialize.php'); 
-  include(SHARED_PATH . '\public-header.php');
+  require_once('../private/initialize.php'); 
+  include(SHARED_PATH . '/public-header.php');
 ?>
     <main>
       <h1>About</h1>
